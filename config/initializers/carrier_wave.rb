6e79e0f2f1d389428700978068a0cddb2c07901f
Abhinav Chaturvedi <abhinav.c@live.in>
2017-08-01 01:32:19 +0530

    fixed carrierwave initializer

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJG7EN5Y46LIWYPFQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['com.abhinavchat']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
